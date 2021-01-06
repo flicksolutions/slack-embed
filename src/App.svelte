9120b9b3aee85512f6988e6cc90c1992daeec870
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2021-01-06 18:05:00 +0100

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -92,20 +92,23 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
+		max-width: 700px;
 	}
 
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 2em;
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	ul{
+		list-style-type: none;
+		text-align: left;
+		padding: 0;
+	}
+	li{
+		margin: .8em 0;
 	}
 </style>
